bb1d5225ae67833367e290321dc59a15530dd1d5
BRYAN CALLE <bryanjeffersoncallechavez@gmail.com>
2026-05-31 13:12:47 -0500

    Delete TRABAJO GRUPAL HTML Y CSS 2 ACTUALIZADO directory

--- TRABAJO GRUPAL HTML Y CSS 2 ACTUALIZADO/estilosscript/alertas.js
+++
@@ -1,4 +0,0 @@
-//--alertas.js  
-alert("Bienvenido a nuestra tienda");
-alert("Puedes seleccionar nuestros productos haciendo uso de checkboxes, y luego ir a la bolsa 🛍️ de compras para finalizar.");
-alert("¡TENEMOS DESCUENTOS POR COMPRAS A MAYOR DE $300! 🛍️");
